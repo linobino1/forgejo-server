1e6ba01f108be32793e7b5de22fc5ddbd279ed79
Leo <leo.hilsheimer@gmail.com>
2025-12-05 10:22:57 +0100

    backup script: do not backup backups

--- backup.sh
+++ backup.sh
@@ -19,7 +19,8 @@ echo "[*] Starting Forgejo backup: $BACKUP_FILE"
 docker exec --user git "$CONTAINER" forgejo dump --type tar.gz --file "/data/backup-$DATE.tar.gz"
 
 # 2. Move the tarball from container /data to host backup directory
-# (Because /data is bind-mounted to /opt/apps/forgejo-server/forgejo)
+# remove any existing backups, we don't want to backup backups
+rm -f "$BACKUP_DIR"/*.tar.gz
 mv "/opt/apps/forgejo-server/forgejo/backup-$DATE.tar.gz" "$BACKUP_FILE"
 
 echo "[*] Forgejo backup completed."
